8c85dd071e1e68b13e6869a0c7b2b50f79ceebae
Stuti Dobhal <stuti@7mind.de>
2020-12-17 14:30:12 +0100

    Initial Commit

---
+++ HapticsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HapticsDemo
+//
+//  Created by Stuti Dobhal on 17.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
